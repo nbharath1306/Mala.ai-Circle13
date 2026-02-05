3e5a93d536cc1c058dee29fb5a6069412b381c18
N Bharath <n.bharath3430@gmail.com>
2026-02-05 18:43:57 +0530

    chore: Clear repository content

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import BeadCounter from '@/components/BeadCounter';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-black">
-      <BeadCounter />
-    </main>
-  );
-}
